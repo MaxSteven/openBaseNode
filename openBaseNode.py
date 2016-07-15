3627a42521dc19eccd573c3cd9b5fcba24767466
greenvfx <latest.green@gmail.com>
2016-07-15 23:00:38 +0300

    Add list knob support

--- openBaseNode.py
+++ openBaseNode.py
@@ -1,11 +1,39 @@
 import nuke
+import re
 
-def openBaseNode(float = True):
-    sn = nuke.selectedNode()
-    for knob in sn.knobs():
-        if sn['%s'%knob].hasExpression():
-            expression = sn['%s'%knob].animation(0).expression()
-            expression = expression.replace("parent.", "")
-            nodeName = expression.split('.')
-            n = nuke.toNode('%s'%nodeName[0])
-            nuke.show(n, forceFloat = float)
+def findAllNodes(expression):
+    curNodeList = []
+    expression = expression.replace("parent.", "")
+    nodeKnob = re.compile('[A-Za-z0-9_.]+')
+    findedNodes = nodeKnob.findall(expression)
+    for findedNode in findedNodes:
+        nodeName = findedNode.split('.')
+        curNodeList.append(nodeName[0])
+    return curNodeList
+
+def openBaseNode(openFloat = True):
+    nodeList = []
+    nodes = nuke.selectedNodes()
+    for sn in nodes:
+        for knob in sn.knobs():
+            curKnob = sn['%s'%knob]
+            if curKnob.hasExpression():
+                knobValue = curKnob.getValue()
+                knobType = type(knobValue)
+                if knobType is float:
+                    curFloatExpression = curKnob.animation(0).expression()
+                    foundNodesFloat = findAllNodes(curFloatExpression)
+                    nodeList.extend(foundNodesFloat)
+                if knobType is list:
+                    for i in range(0, len(knobValue)):
+                        try:
+                            curListExpression =  curKnob.animation(i).expression()
+                            foundNodesList = findAllNodes(curListExpression)
+                            nodeList.extend(foundNodesList)
+                        except:
+                            pass
+
+    nodeList = list(set(nodeList))
+    for node in nodeList:
+        n = nuke.toNode('%s'%node)
+        nuke.show(n, forceFloat = openFloat)
